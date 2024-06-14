61107cf752431148d2e999e1da4c9601c3bc7930
Zoselro <zoselro0328@gmail.com>
2024-06-14 17:36:26 +0900

    20240614 17:33 commit

--- Chapter93_96_File_Function/main.cpp
+++ Chapter93_96_File_Function/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main(){
     ifstream read;
     cout << "-----------한 글자씩 읽기------------" << endl;
-    read.open("D:/Visual Code C++ 200/Chapter93_103_File_Function/093.txt", 
+    read.open("D:/Visual Code C++ 200/Chapter93_96_File_Function/093.txt", 
     ifstream::in);
 
     if (!read.is_open()){
@@ -25,7 +25,7 @@ int main(){
     read.close();
 
     cout << "-------한 줄 씩 읽기-------" << endl;
-    read.open("D:/Visual Code C++ 200/Chapter93_103_File_Function/093.txt", 
+    read.open("D:/Visual Code C++ 200/Chapter93_96_File_Function/093.txt", 
     ifstream::in);
 
     string lineS; // 파일을 한 줄 씩 읽기
@@ -34,11 +34,11 @@ int main(){
     }
 
     read.close();
-    
-    cout << "파일 내용을 모두 읽기" << endl;
+    cout << endl;
+    cout << "파일 내용을 모두 읽기";
 
     //파일을 읽기 위한 입력 스트림
-    ifstream ifs("D:/Visual Code C++ 200/Chapter93_103_File_Function/093.txt"
+    ifstream ifs("D:/Visual Code C++ 200/Chapter93_96_File_Function/093.txt"
     , ifstream::in); // 읽기모드로 파일 Open
 
     stringstream ss; // stringstream 객체 ss 생성
@@ -48,12 +48,8 @@ int main(){
 
     cout << endl << read1 << endl; // 결과 출력
     
-    read.close() // 파일 닫기
-    
-    cout << "------ 파일 쓰기 -----" << endl; 
-    
-    
-    
+    read.close(); // 파일 닫기
+
     return 0;
 }
 
