90d56b149ab7bca0627b9f404c41c17ed93c0c47
Zoselro <zoselro0328@gmail.com>
2024-09-05 10:23:08 +0900

    Chapter126_HEXA_ADD

--- Chapter126_HEXA_ADD/hexaadd.cpp
+++ Chapter126_HEXA_ADD/hexaadd.cpp
@@ -20,7 +20,7 @@ int HexaAdd::NOT(int x){
 }
 
 int HexaAdd::XOR(int x, int y){
-    int z = x ^ y;
+    int z = (x | y) & ~(x & y);
     return z;
 }
 
@@ -28,9 +28,34 @@ void HexaAdd::sub(int x[8], int y[8], int out[8]){
 
     int one[8] = {0, 0, 0, 0, 0, 0, 0, 1};
     int sum[8];
+    int carry = 0;
 
     for(int i = 0 ; i < 8 ; i++){
         y[i] = NOT(y[i]); 
         sum[i] = XOR(y[i],one[i]); // 1의 보수 값 더함
+    }
+
+    for(int i = 7 ; i >= 0 ; i--){
+        carry = sum[i] + x[i] + carry;
+
+        if(carry == 0){
+            out[i] = 1;
+        }
+        if(carry == 1){
+            out[i] = 1;
+            carry = 0;
+        }
+        if(carry == 2){
+            carry /= 2;
+            out[i] = 0;
+        }
+        if(carry == 3){
+            carry /= 2;
+            out[i] = 1;
+        }
+    }
+
+    for(int i = 0 ; i < 8 ; i++){
+        cout << out[i] << " ";
     }
 }
--- Chapter126_HEXA_ADD/main.cpp
+++ Chapter126_HEXA_ADD/main.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(){
-    int x[8] = {0, 0, 0, 1, 1, 0, 1, 1}; // 27
-    int y[8] = {0, 0, 0, 1, 1, 0, 0, 1}; // 25
+    int x[8] = {1, 0, 0, 1, 1, 0, 1, 1}; // 155
+    int y[8] = {1, 1, 0, 1, 1, 0, 0, 1}; // 217
     int z[8];
-    
+
     HexaAdd hx;
     
     hx.sub(x,y,z);
-    
+
     return 0;
 }
