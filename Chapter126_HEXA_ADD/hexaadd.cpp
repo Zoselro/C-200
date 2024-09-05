b1a509c550f60a45890728d378b32f1b4d7e46e1
Zoselro <zoselro0328@gmail.com>
2024-09-05 13:28:51 +0900

    2024-09-05 hexaadd

--- Chapter126_HEXA_ADD/hexaadd.cpp
+++ Chapter126_HEXA_ADD/hexaadd.cpp
@@ -24,38 +24,80 @@ int HexaAdd::XOR(int x, int y){
     return z;
 }
 
-void HexaAdd::sub(int x[8], int y[8], int out[8]){
+void HexaAdd::sub(int x[8], int y[8]){
 
+    int out[8] = {};
     int one[8] = {0, 0, 0, 0, 0, 0, 0, 1};
-    int sum[8];
+    int sum[8] = {};
+    int y_complement[8] = {};
     int carry = 0;
+    int size = sizeof(one) / sizeof(int);
 
-    for(int i = 0 ; i < 8 ; i++){
+    for(int i = 0 ; i < size ; i++){
         y[i] = NOT(y[i]); 
-        sum[i] = XOR(y[i],one[i]); // 1의 보수 값 더함
+        //sum[i] = XOR(y[i],one[i]); // 1의 보수 값 더함
     }
 
-    for(int i = 7 ; i >= 0 ; i--){
-        carry = sum[i] + x[i] + carry;
+    for(int i = size-1 ; i >= 0 ; i--){
+        carry = y[i] + one[i] + carry;
 
         if(carry == 0){
-            out[i] = 1;
+            if(y[i] == 0 && one[i] == 0)
+                y_complement[i] = 0;
+            else
+                y_complement[i] = 1;
+        }
+
+        if(carry == 1){
+            y_complement[i] = 1;
+            carry /= 2;
+        }
+
+        if(carry == 2){
+            y_complement[i] = 0;
+            carry /= 2;
+        }
+    }
+
+    for(int i = 0 ; i < size ; i++){
+        cout << y_complement[i] << " ";
+    }
+    cout << endl;
+
+    for(int i = 0 ; i < size ; i++){
+        cout << x[i] << " ";
+    }
+    cout << endl;
+
+    carry = 0;
+
+    for(int i = size - 1 ; i >= 0 ; i--){
+        carry = y_complement[i] + x[i] + carry;
+
+        if(carry == 0){
+            if(y_complement[i] == 0 && x[i] == 0){
+                out[i] = 0;
+
+            }
+            else{
+                out[i] = 1;
+
+            }
         }
         if(carry == 1){
             out[i] = 1;
-            carry = 0;
+            carry /= 2;
         }
         if(carry == 2){
             carry /= 2;
             out[i] = 0;
         }
         if(carry == 3){
-            carry /= 2;
             out[i] = 1;
+            carry /= 2;
         }
     }
-
-    for(int i = 0 ; i < 8 ; i++){
+    for (int i = 0 ; i < size ; i++){
         cout << out[i] << " ";
     }
 }
--- Chapter126_HEXA_ADD/main.cpp
+++ Chapter126_HEXA_ADD/main.cpp
@@ -5,12 +5,10 @@ using namespace std;
 
 int main(){
     int x[8] = {1, 0, 0, 1, 1, 0, 1, 1}; // 155
-    int y[8] = {1, 1, 0, 1, 1, 0, 0, 1}; // 217
-    int z[8];
+    int y[8] = {1, 1, 1, 1, 0, 1, 1, 1}; // 217
 
     HexaAdd hx;
-    
-    hx.sub(x,y,z);
+    hx.sub(x,y);
 
     return 0;
 }
